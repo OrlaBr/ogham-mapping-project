fbcfac6d2d4e2380a4ed634785544b1ca4747e9f
OrlaBr <orlabreslin@yahoo.com>
2020-01-28 15:48:01 +0000

    Fixed infowindow error - width

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,12 @@ function initMap() {
           },
           {coords:{lat:52.1301, lng: -10.4503},
           content:'<h1>Dunquin</h1>'
-          }
+          },
+        {
+         coords:{lat:52.12675, lng: -10.30457},
+         content:'<img src="oghamstone.jpg" alt="ogham stone"><p><strong>Ballinrannig</strong> - Not in its original location, currently on the grounds of Colaiste Ide, Burnham, with other re-located ogham stones</p><a href="#" class="btn btn-default btn-sm iwButton">Read more about Ballinrannig</a>',
+         draggable: false
+         },
  
         ];
           // Loop through markers
@@ -51,7 +56,7 @@ function initMap() {
         //check content
         if(props.content){
           var infoWindow = new google.maps.InfoWindow({
-            content:props.content
+            content:props.content, maxWidth: 300
       });
         marker.addListener('click', function(){
         infoWindow.open(map, marker);
