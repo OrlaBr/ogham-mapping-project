b07edccffb3aa283e420ef51cd873eb6d3927917
OrlaBr <orlabreslin@yahoo.com>
2020-01-28 15:35:23 +0000

    Editing map markers and responsive sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,34 @@
-function initMap(){
+function initMap() {
         var options = {
           center: {lat:52.1409, lng: -10.2640},
-          zoom:8,
+          zoom: 8,
           mapTypeId: google.maps.MapTypeId.HYBRID,
-          minZoom: 9,
+          minZoom: 9,   
           zoomControlOptions: {
             position: google.maps.ControlPosition.LEFT_BOTTOM, 
             style: google.maps.ZoomControlStyle.SMALL
           },
           panControlOptions: {
             position: google.maps.ControlPosition.LEFT_BOTTOM
-          },
-          
-        };
+          }
+        }
         var map = new google.maps.Map(document.getElementById('map'), options);
+    
       //Array of Markers
         var markers = [
-          {
+           {
           coords:{lat:52.12675, lng: -10.30457},
-          iconImage:'assets/img/map-icon.png',
-          content:'<h5><a href="#">Ballinrannig</a></h5>',
-          draggable: false
+          iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+          content:'<h1>burnham</h1>'
           },
           {
-          coords:{lat:52.15705, lng: -10.2517},
-          content:'<h3>Baile an Ásaigh (Ballinasig) </h3>',
-          draggable: false
+          coords:{lat:52.2174, lng: -10.3402},
+          content:'<h1>Feohanagh</h1>'
           },
-          {
-          coords:{lat:52.14890, lng: -10.4471},
-          content:'<h3>Baile Ristín (Ballyrishteen)</h3>',
-          draggable: false
-          },
-          {
-          coords:{lat:52.2103, lng: -9.9496},
-          content:'<h3>Gleann na nGealt</h3>',
-          draggable: false
-          },
-          {
-          coords:{lat:52.1674, lng: -10.3875},
-          content:'<h3>An Riasc (Reask)</h3>',
-          draggable: false
-          },
+          {coords:{lat:52.1301, lng: -10.4503},
+          content:'<h1>Dunquin</h1>'
+          }
+ 
         ];
           // Loop through markers
           for(var i =0;i < markers.length;i++){
@@ -49,7 +36,7 @@ function initMap(){
               addMarker(markers[i]);
           }
 
-        //Add Marker function
+        //Add Marker function - (better practice  - Google Maps Garage)
       function addMarker(props){
         var marker = new google.maps.Marker({
         position:props.coords,
@@ -68,8 +55,11 @@ function initMap(){
       });
         marker.addListener('click', function(){
         infoWindow.open(map, marker);
-      });
+      });      
+      //added a resize-event for responsive infoWindow 
+        google.maps.event.addDomListener(window, 'resize', function() {
+          infowindow.open(map);
+        });
         }
       }
-    }
-    
+    };
