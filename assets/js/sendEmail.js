ab5335e615c3dd9015aa0bd556565c962344e4d8
OrlaBr <orlabreslin@yahoo.com>
2020-02-04 11:26:34 +0000

    Connect EmailJS to contact form

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+  email.js.send("gmail", "ogham-mapping", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "contact_message": contactForm.contactsummary.value
+  })
+    .then(
+        function(response) {
+          console.log("Success", response);
+        },
+        function(error) {
+          console.log("Failled", error);
+        });
+}
