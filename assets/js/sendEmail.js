bb3687a2b6c68a5e56f6b2d51cb175eefe8f15b9
OrlaBr <orlabreslin@yahoo.com>
2020-02-04 13:45:16 +0000

    Fixed resposive centering issues on About page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,15 @@
 function sendMail(contactForm) {
-  email.js.send("gmail", "ogham-mapping", {
-      "from_name": contactForm.name.value,
-      "from_email": contactForm.emailaddress.value,
-      "contact_message": contactForm.contactsummary.value
-  })
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.message.value
+    })
     .then(
         function(response) {
-          console.log("Success", response);
+            console.log("SUCCESS", response);
         },
         function(error) {
-          console.log("Failed", error);
-        });
+            console.log("FAILED", error);
+        }
+    );
 }
